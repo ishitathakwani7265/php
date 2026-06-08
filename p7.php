ed352dadfe2aca15de7c14d0daad26d3dda3cc0d
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-06-08 12:26:52 +0530

    Add files via upload

---
+++ p7.php
@@ -0,0 +1,15 @@
+<?php
+	$fruit="mango";
+	if($fruit=="mango")
+	{
+		echo"it is summer fruit";
+	}
+	else if($fruit=="apple")
+	{
+		echo"it is all seasom fruit";
+	}
+	else
+	{
+		echo"it is not a fruit";
+	}
+?>
