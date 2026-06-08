26ddd2bfebdffae4ce6415e486b67f5f083d7dcf
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-06-08 12:27:03 +0530

    Add files via upload

---
+++ p8.php
@@ -0,0 +1,19 @@
+<?php
+	$college="marwadi";
+	$course="bca";
+	if($college="marwadi")
+	{
+		if($course=="bca")
+		{
+			echo"10% discount to student";
+		}
+		else
+		{
+			echo"They are not in BCA";
+		}
+	}
+	else
+	{
+		echo"They didn't select Marwadi";
+	}
+?>
