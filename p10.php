9746ec04cf6d9cfd9e25dc2c515a173907af915e
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-06-12 12:28:22 +0530

    Add files via upload

---
+++ p10.php
@@ -0,0 +1,14 @@
+/* 1.2 Write a PHP program to find out maximum and minimum number */
+<?php
+
+$num1 = 45;
+$num2 = 78;
+
+if ($num1 > $num2) {
+    echo "Maximum Number = " . $num1 . "<br>";
+    echo "Minimum Number = " . $num2;
+} else {
+    echo "Maximum Number = " . $num2 . "<br>";
+    echo "Minimum Number = " . $num1;
+}
+?>
