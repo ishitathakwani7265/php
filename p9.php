fbfba9021cfd5d8c165f9948ed0c8a0d09eca990
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-06-12 12:37:54 +0530

    Add files via upload

---
+++ p9.php
@@ -0,0 +1,14 @@
+/* 1.1 Write a PHP program for print Previous semester Result using variables & constants in PHP. */
+<?php
+
+define("COLLEGE_NAME", "Marwadi University");
+
+$studentName = "Ishita";
+$semester = "Semester 4";
+$pr = 8.20;
+
+echo "College Name: " . COLLEGE_NAME . "<br>";
+echo "Student Name: " . $studentName . "<br>";
+echo "Semester: " . $semester . "<br>";
+echo "Result: " . $pr . "pr" ;
+?>
