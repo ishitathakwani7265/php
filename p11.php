e9f0cccf860b033a18ca9c3780a5a625776378e8
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-06-12 12:28:34 +0530

    Add files via upload

---
+++ p11.php
@@ -0,0 +1,12 @@
+/* 1.3 Write a PHP program for operators in PHP. */
+<?php
+
+$a = 20;
+$b = 10;
+
+echo "Addition = " . ($a + $b) . "<br>";
+echo "Subtraction = " . ($a - $b) . "<br>";
+echo "Multiplication = " . ($a * $b) . "<br>";
+echo "Division = " . ($a / $b) . "<br>";
+echo "Modulus = " . ($a % $b);
+?>
