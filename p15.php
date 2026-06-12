cfce2b76fa8b6b1ce297dab65a6e1d49261af953
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-06-12 12:38:26 +0530

    Add files via upload

---
+++ p15.php
@@ -0,0 +1,9 @@
+/* 1.7 Write a PHP program to include a file using include and require function. */
+<?php
+
+include("header.php");
+
+echo "Welcome to Marwadi University<br>";
+
+require("footer.php");
+?>
