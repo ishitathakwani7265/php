d877d8b8f1086d0a644fe7b1d516d371cf908dc7
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-06-22 12:40:21 +0530

    Add files via upload

---
+++ p30.php
@@ -0,0 +1,9 @@
+<?php
+date_default_timezone_set('asia/kolkata');
+echo "today is " .date("y/m/d")."<br/>";
+echo "today is " .date("d/m/y")."<br/>";
+echo "today is " .date("d-m-y  h.i.s.a")."<br/>";
+echo "today is " .date("l")."<br/>";
+
+
+?>
