ab3306621d5ee6b494f0f983bf32d8686e141121
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-06-22 12:39:27 +0530

    Add files via upload

---
+++ p28.php
@@ -0,0 +1,17 @@
+<?php
+$t1 = "Hello World!";
+echo strrev($t1);
+
+echo "<br/>";
+$text = "Hello Word!";
+echo strlen($text);
+
+echo "<br/>";
+$text = "Hello Word!";
+echo strtolower($text);
+
+echo "<br/>";
+$text = "Hello Word!";
+echo mb_strtoupper($text);
+
+?>
