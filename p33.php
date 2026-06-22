6e3d6452803f3b43f3bb4c864acce23628581e96
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-06-22 12:56:10 +0530

    Add files via upload

---
+++ p33.php
@@ -0,0 +1,9 @@
+<?php
+// 1.8 Write a PHP Program to print the values of array entered by user.
+
+$arr = array("Apple", "Banana", "Mango", "Orange");
+
+foreach ($arr as $value) {
+    echo $value . "<br>";
+}
+?>
