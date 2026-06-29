6b321dbfde59e88ce5946747e10a2474dc7fb418
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-06-29 13:06:25 +0530

    Add files via upload

---
+++ p42.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<form action = "p43.php" method = "post"
+enctype = "multipart/form-data">
+
+Select file: <input type = "file" name="fileToUpload">
+<input type = "submit" value = "Upload image"
+name = "submit">
+
+</form>
+</body>
+</html>
