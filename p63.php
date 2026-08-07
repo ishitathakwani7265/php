8126b2e4d48ab65298fabc3935f2fa3c57f0a948
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-08-07 11:13:52 +0530

    Add files via upload

---
+++ p63.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+
+    <title>AJAX Form Validation</title>
+</head>
+<body>
+    <form id="myForm" action=form1.php method="post">
+        Name: <input type="text" id="name" name="name"><br>
+        Email: <input type="email" id="email" name="email"><br>
+        <input type="submit" value="Submit">
+    </form>
+    <div id="response"></div>
+
+    <script src="script.js"></script>
+</body>
+</html>
